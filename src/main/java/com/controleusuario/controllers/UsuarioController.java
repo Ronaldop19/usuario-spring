9c12b2bbf8910f1f49d385681c07de83b9f8709d
Ronaldo <ronaldop.amorim@hotmail.com>
2025-02-04 17:44:58 -0300

    feat: add methods put and delete, att pom.xml

--- src/main/java/com/controleusuario/controllers/UsuarioController.java
+++ src/main/java/com/controleusuario/controllers/UsuarioController.java
@@ -3,6 +3,7 @@ package com.controleusuario.controllers;
 import com.controleusuario.dtos.UsuarioCreateDTO;
 import com.controleusuario.entity.Usuario;
 import com.controleusuario.repositories.UsuarioRepository;
+import jdk.javadoc.doclet.Reporter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -27,8 +28,36 @@ public class UsuarioController {
     @PostMapping
     public ResponseEntity<String> createUsuario(@RequestBody @Validated UsuarioCreateDTO usuarioCreateDTO){
         Usuario newUsuario = new Usuario(usuarioCreateDTO);
-        System.out.println(usuarioCreateDTO);
         usuarioRepository.save(newUsuario);
         return ResponseEntity.ok().build();
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateUsuario(@PathVariable Long id, @RequestBody @Validated UsuarioCreateDTO usuarioUpdateDTO) {
+        if (!usuarioRepository.existsById(id)) {
+            return ResponseEntity.status(404).body("Usuário não encontrado!");
+        }
+
+        Usuario usuario = usuarioRepository.getReferenceById(id);
+        usuario.setPrimeiroNome(usuarioUpdateDTO.primeiroNome());
+        usuario.setUltimoNome(usuarioUpdateDTO.ultimoNome());
+        usuario.setCargo(usuarioUpdateDTO.cargo());
+        usuario.setEmail(usuarioUpdateDTO.email());
+
+        usuarioRepository.save(usuario);
+
+        return ResponseEntity.ok("Usuário atualizado com sucesso!");
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUsuario(@PathVariable Long id) {
+        if (!usuarioRepository.existsById(id)) {
+            return ResponseEntity.status(404).body("Usuário não encontrado!");
+        }
+
+        usuarioRepository.deleteById(id);
+        return ResponseEntity.ok("Usuário deletado com sucesso!");
+    }
+
 }
--- src/main/java/com/controleusuario/dtos/UsuarioCreateDTO.java
+++ src/main/java/com/controleusuario/dtos/UsuarioCreateDTO.java
@@ -1,5 +1,22 @@
 package com.controleusuario.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record UsuarioCreateDTO(String primeiroNome, String ultimoNome, String cargo, String email){
-}
+public record UsuarioCreateDTO(
+        Long id,
+
+        @NotBlank(message = "O primeiro nome não pode estar vazio.")
+        @Size(min = 2, max = 50, message = "O primeiro nome deve ter entre 2 e 50 caracteres.")
+        String primeiroNome,
+
+        @Size(max = 50, message = "O sobrenome deve ter no máximo 50 caracteres.")
+        String ultimoNome,
+
+        @Size(max = 100, message = "O cargo deve ter no máximo 100 caracteres.")
+        String cargo,
+
+        @NotBlank(message = "O email é obrigatório.")
+        @Size(max = 100, message = "O email deve ter no máximo 100 caracteres.")
+        String email
+) {}
